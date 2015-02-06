22426c9c22b92729ce6a9aeb9a49862ad06742d4
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-06 14:34:06 -0500

    create a user dashboard once user is saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
 
+  after_save :create_dashboard
+
   has_one :dashboard
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+private
+
+  def create_dashboard
+    Dashboard.create(user_id: self.id)
+  end
+
 end
