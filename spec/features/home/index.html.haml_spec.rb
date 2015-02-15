fbcc2b3652ccc4dec9215bd3336049d2882c0252
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-15 17:13:36 -0500

    test: use root_path

--- spec/features/home/index.html.haml_spec.rb
+++ spec/features/home/index.html.haml_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 describe 'Home' do
 
   it 'will have a logo name SusuBank' do
-    visit '/'
+    visit root_path
     expect(page).to have_content('SusuBank')
   end
   it 'will have an About Us link' do
-    visit '/'
+    visit root_path
     expect(page).to have_content('About Us')
   end
 
   it 'will have a Contact Us link' do
-    visit '/'
+    visit root_path
     expect(page).to have_content('Contact Us')
   end
 
   it 'will have a Learn More link' do
-    visit '/'
+    visit root_path
     expect(page).to have_content('Learn More')
   end
 
