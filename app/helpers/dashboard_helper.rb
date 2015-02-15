f3af7f6f766f4b7a625155217b899c8abdd2a7d1
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-15 00:04:39 -0500

    add a sign in "counter"

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,4 +4,13 @@ module DashboardHelper
     "#{Time.now.strftime('%A, %B %d, %Y')}"
   end
 
+  def visit_count
+    last_sign_in = Time.now - current_user.last_sign_in_at
+    mins = last_sign_in.to_i / 60
+    if current_user.sign_in_count == 1
+      "Welcome aboard"
+    else
+      "You last signed in #{time_ago_in_words(mins.minutes.ago)} ago."
+    end
+  end
 end
