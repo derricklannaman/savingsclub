b3202fe5759b085c8f137d4c3440f7bc62bd6e83
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-15 18:31:51 -0500

    dashboard helper refactor

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -5,12 +5,22 @@ module DashboardHelper
   end
 
   def visit_count
-    last_sign_in = Time.now - current_user.last_sign_in_at
-    mins = last_sign_in.to_i / 60
-    if current_user.sign_in_count == 1
+    mins = calculate_minutes_since_sign_in
+    if first_sign_in?
       "Welcome aboard"
     else
       "You last signed in #{time_ago_in_words(mins.minutes.ago)} ago."
     end
   end
+
+  def calculate_minutes_since_sign_in
+    last_sign_in = Time.now - current_user.last_sign_in_at
+    last_sign_in.to_i / 60
+  end
+
+  def first_sign_in?
+    current_user.sign_in_count == 1
+  end
+
+
 end
