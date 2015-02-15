5f35f5a0c780f738feccb93c29ee2b0d49944c26
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-14 20:10:25 -0500

    add first and last name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controller: { registrations: 'registrations' }
+
   root 'home#index'
 
   get 'about', to: 'home#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207142445) do
+ActiveRecord::Schema.define(version: 20150215010412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "dashboards", force: true do |t|
+  create_table "dashboards", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -24,29 +24,31 @@ ActiveRecord::Schema.define(version: 20150207142445) do
 
   add_index "dashboards", ["user_id"], name: "index_dashboards_on_user_id", using: :btree
 
-  create_table "susus", force: true do |t|
+  create_table "susus", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "name",        limit: 255
     t.decimal  "total_value"
     t.integer  "user_id"
   end
 
   add_index "susus", ["user_id"], name: "index_susus_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
