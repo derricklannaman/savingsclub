6a50d9015fca78817c6fcd20c575421d41adffc7
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-15 22:11:20 -0500

    small view refactor

--- app/helpers/susus_helper.rb
+++ app/helpers/susus_helper.rb
@@ -1,2 +1,16 @@
 module SususHelper
+
+  def format_start_date(susu)
+    "Begins: #{susu.start_date.to_time.strftime('%m/%d/%Y')}"
+  end
+
+  def display_member_count(susu)
+    "#{susu.member_count} Members"
+  end
+
+  def display_payout(susu)
+    "Payout: #{number_to_currency(susu.total_value)}"
+  end
+
+
 end
