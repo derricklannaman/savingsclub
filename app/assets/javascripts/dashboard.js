c19b89602bd00c6723520cf9ea58901520e1942d
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-16 18:48:29 -0500

    add shadows to dashboard buttons on hover

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,2 +1,17 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $('.dashboard-button').hover(raisePushButton, lowerPushButton);
+});
+
+function raisePushButton() {
+  var btn = $( this );
+  if (btn.hasClass('recessed')) {
+    btn.removeClass('recessed');
+  }
+  btn.addClass('levitate', 3000);
+}
+
+function lowerPushButton() {
+  var btn = $( this );
+  btn.removeClass('levitate');
+}
+
