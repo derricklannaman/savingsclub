79b50359775f5d5243e9f8a51377700dfd27d4de
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-19 22:15:06 -0500

    add member controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,27 @@
+class MembersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
