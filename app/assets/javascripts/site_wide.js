8df94a921254745f234b90666a8d2b4c2456f1ea
Derrick Lannaman <derricklannaman@gmail.com>
2015-02-21 00:42:32 -0500

    WIP - message bar

--- app/assets/javascripts/site_wide.js
+++ app/assets/javascripts/site_wide.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-  $('#message_panel')
+  // $('#message_panel').hide(function());
+  $('#message_panel').on('load', check_for_message);
 
 
 });
+
+function check_for_message() {
+  alert("This works!");
+}
