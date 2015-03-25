cdbb680f01785afc9130980daebfa705b38f3599
Derrick Lannaman <derricklannaman@gmail.com>
2015-03-24 23:44:53 -0400

    first pass - "clear view of susu"

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $('.dashboard-button').hover(raisePushButton, lowerPushButton);
+  $('#clear-view_btn').on('click', hideBankerNavigation);
 });
+
+
+function hideBankerNavigation() {
+  if( $('body').hasClass('susu' && 'show') ) {
+    $('body').fadeOut(1000);
+    setTimeout(function(){
+      $('#banker_control_panel').hide();
+    }, 500)
+
+
+  }
+
+  // $('#banker_control_panel').fadeToggle(500);
+}
 
 function raisePushButton() {
   var btn = $( this );
