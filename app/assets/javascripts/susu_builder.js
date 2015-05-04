aaf13f7cc8fdba9fb0bbde84b95977e3d4c5caad
Derrick Lannaman <derricklannaman@gmail.com>
2015-05-03 22:00:01 -0400

    start calculate results work

--- app/assets/javascripts/susu_builder.js
+++ app/assets/javascripts/susu_builder.js
@@ -5,5 +5,8 @@ $(document).ready(function(){
 });
 
 function calculateSusuBankNumbers() {
-  var members = $('#susu_member_count').val();
+  var members = parseInt( $('#susu_member_count').val() );
+  var susuValue = parseInt( $('#susu_total_value').val() );
+  var contributeAmount = susuValue / members;
+  $('#contribution-results').append(contributeAmount)
 }
