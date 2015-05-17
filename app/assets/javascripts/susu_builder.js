942f0cb73b320d9acf5873d5088f1bf3d3191d61
Derrick Lannaman <derricklannaman@gmail.com>
2015-05-17 14:37:14 -0400

    Start to flesh out result panel

--- app/assets/javascripts/susu_builder.js
+++ app/assets/javascripts/susu_builder.js
@@ -8,7 +8,7 @@ function calculateSusuBankNumbers() {
   var members = parseInt( $('#susu_member_count').val() );
   var susuValue = parseInt( $('#susu_total_value').val() );
   var contributeAmount = susuValue / members;
-  var everyWeek = "<span>Every Week</span>";
+  var everyWeek = "<span>Every Week $</span>";
 
   $('#per-period').append(everyWeek)
   $('#contribution-amount').append(contributeAmount)
