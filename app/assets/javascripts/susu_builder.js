331d2f0b637dad707060a1679fb97df51affd7b2
Derrick Lannaman <derricklannaman@gmail.com>
2015-05-17 20:33:12 -0400

    create color sections in results area

--- app/assets/javascripts/susu_builder.js
+++ app/assets/javascripts/susu_builder.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-
+  // $('#results-header').hide();
   $('#calculate').on('click', calculateSusuBankNumbers);
-
+  // $('#results-header p').fadeOut();
 });
 
 function calculateSusuBankNumbers() {
   if( $('#susu-title').text().length > 0) {
     $('#susu-title').empty();
+    $('#total-payout').empty();
   }
 
   var members = parseInt( $('#susu_member_count').val() );
   var susuValue = parseInt( $('#susu_total_value').val() );
   var contributeAmount = susuValue / members;
   var susuName = $('#susu_name').val();
-
+  $('#result-title').css('color', '#FFFFFF');
   $('#susu-title').append(susuName);
+  $('#total-payout').append(susuValue);
+  $('#contribution-period').append(contributeAmount);
 }
